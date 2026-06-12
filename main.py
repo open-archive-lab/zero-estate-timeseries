aa36f60c47c6a252b2972023b2772781ceafd03f
BAC <41997195+bac0id@users.noreply.github.com>
2026-06-12 21:44:28 +0800

    Fix pipeline

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import asyncio
 
-from download_options import download_options
-from extract_options import extract_options
 from download_results import download_results
 from extract_results import extract_results
 
 
 async def main():
-    await download_options()
-    await extract_options()
     await download_results()
     await extract_results()
 
